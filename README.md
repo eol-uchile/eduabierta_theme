93a0a720e4bd360ca3fdbd0f74ae6f82182ad641
Felipe Espinoza <felipe.espinoza.r@uchile.cl>
2020-03-12 19:01:23 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Eduabierta Theme
+
+Eduabiera theme for [eduabierta.uchile.cl](eduabierta.uchile.cl). This theme must be used as *eduabierta* in OpenedX.
---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="eduabierta_register",
+    version="0.0.1",
+    author="fdns",
+    author_email="felipe.espinoza.r@uchile.cl",
+    description="EduAbierta registration form",
+    long_description="EduAbierta registration form",
+    url="https://eduabierta.uchile.cl",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
